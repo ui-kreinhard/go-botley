cdb1b39dd14f585d3df59db10235029d78e75f4a
Karl-Ludwig Reinhard <reinhard@synyx.de>
2024-09-14 12:39:00 +0200

    Add new movements
    * Forward
    * Rotate left 90

--- command/defintions.go
+++ command/defintions.go
@@ -1,9 +1,17 @@
 package command
 
+var Forward Burst = Burst{
+	parts: []Part{StartOfBurst, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, One},
+}
+
 var Backward Burst = Burst{
 	parts: []Part{StartOfBurst, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, One, Mark, Zero},
 }
 
-var Rotate90 Burst = Burst{
+var RotateRight90 Burst = Burst{
 	parts: []Part{StartOfBurst, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, One, Mark, Zero, Mark, Zero},
+}
+
+var RotateLeft90 Burst = Burst{
+	parts: []Part{StartOfBurst, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, Zero, Mark, One, Mark, One},
 }
